75355b3222448261d59a20560a3500bf9fa653c7
Antenor <nanaosei452@gmail.com>
2021-08-04 16:46:24 +0000

    Remove material app  from splash

--- lib/pages/Splash.dart
+++ lib/pages/Splash.dart
@@ -5,19 +5,17 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.orangeAccent, Colors.orange, Colors.red],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('Firemed'), CircularProgressIndicator()],
-            )),
-      ),
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.orangeAccent, Colors.orange, Colors.red],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Firemed'), CircularProgressIndicator()],
+          )),
     );
   }
 }
